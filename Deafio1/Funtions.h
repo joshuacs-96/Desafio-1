3d26a42aaf69aa5ce60b4c94e9049a37e9cbf041
Joshua Castro <joshua.csolarte@udea.edu.co>
2025-09-28 23:18:29 -0500

    Video e Informe

--- Deafio1/Funtions.h
+++ Deafio1/Funtions.h
@@ -1,10 +1,11 @@
 #ifndef FUNTIONS_H
 #define FUNTIONS_H
 typedef unsigned char Byte;
-//Manjeo de bits
+//Algoritmos principales
 int pareceRLE_ASCII(const Byte* data, int len);
 char* descomprimirRLE_ASCII(const Byte* data, int len, int& outLen);
 char* descomprimirLZ78(const Byte* compressed, int len, int& outLen);
+//Manjeo de bits
 Byte rotateRight(Byte b, int n);
 Byte* desencriptar(const Byte* encrypted, int len, int n, Byte k);
 //Lectrua
